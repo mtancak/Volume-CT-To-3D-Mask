a89949d6768853752e38708d95fcac25d7b0b3c7
Milan Tancak <tancak.milan@gmail.com>
2022-02-16 22:33:12 +0000

    Added loading and rendering of stls

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import vtk
 import sys
+import os
 from enum import Enum
 
 
@@ -8,11 +9,13 @@ class InputType(Enum):
     DICOM = "dicom"
 
 
-input_type = InputType.NUMPY
+input_type = InputType.DICOM
 input_dir = None
 output_dir = None
 classes = []
 
+input_data = {}
+classes_data = {}
 
 def commands_print():
     global commands
@@ -87,15 +90,64 @@ def delete_class():
         print("Class index does not exist. (hint: type 'list classes')")
 
 
+def displayPolyData(poly):
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputData(poly)
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+
+    renderer = vtk.vtkRenderer()
+    renderer.AddActor(actor)
+
+    render_window = vtk.vtkRenderWindow()
+    render_window.AddRenderer(renderer)
+
+    rwi = vtk.vtkRenderWindowInteractor()
+    rwi.SetRenderWindow(render_window)
+
+    render_window.Render()
+    render_window.Start()
+    rwi.Start()
+
+
+def process():
+    global input_type
+    global input_dir
+    global classes
+
+    get_list_of_inputs = os.listdir(input_dir)
+    print("Detected (" + str(len(get_list_of_inputs)) + ") inputs. ")
+
+    for i, class_dir in enumerate(classes):
+        class_inputs = os.listdir(class_dir)
+        print("Detected (" + str(len(get_list_of_inputs)) + ") inputs for class (" + str(i) + "). ")
+
+        input_reader = None
+        if input_type == InputType.DICOM:
+            input_reader = vtk.vtkDICOMImageReader()
+
+        class_reader = vtk.vtkSTLReader()
+
+        for input_entry in get_list_of_inputs:
+            input_reader.SetDirectoryName(input_dir + input_entry)
+            input_reader.Update()
+
+            if input_entry + ".stl" in class_inputs:
+                class_reader.SetFileName(class_dir + input_entry + ".stl")
+                class_reader.Update()
+                displayPolyData(class_reader.GetOutput())
+
 commands = {
     "exit": sys.exit,
     "commands": commands_print,
     "set input type": set_input_type,
     "set input dir": set_input_dir,
     "set output dir": set_output_dir,
-    "create class": create_class,
     "list classes": list_classes,
-    "delete class": delete_class
+    "create class": create_class,
+    "delete class": delete_class,
+    "process": process
 }
 
 if __name__ == "__main__":
