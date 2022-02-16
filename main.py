eae2d0dde37b864ed71663640fafd0675748e7d8
Milan Tancak <tancak.milan@gmail.com>
2022-02-16 19:21:39 +0000

    Added command to set the input file type

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import vtk
 import sys
+from enum import Enum
+
+
+class InputType(Enum):
+    NUMPY = "numpy"
+    DICOM = "dicom"
+
+
+input_type = InputType.NUMPY
 
 
 def commands_print():
@@ -8,9 +17,26 @@ def commands_print():
         print("  " + command)
 
 
+def set_input_type():
+    global input_type
+
+    print("Available types: ")
+    for t in InputType:
+        print("  " + str(t.value))
+    entered_type = input("Set Input Type: ")
+
+    try:
+        read_type = InputType(entered_type)
+        input_type = read_type
+        print("Done.")
+    except ValueError:
+        print("Type doesn't exist.")
+
+
 commands = {
     "exit": sys.exit,
     "commands": commands_print,
+    "set input type": set_input_type,
 }
 
 if __name__ == "__main__":
